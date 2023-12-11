ce734e979a6aba37b97c6a23a8eae76da9d2e22a
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-12-11 13:59:15 -0600

    Creo q solo falta eso de los api para recibir eso del los sensores

--- app/src/main/java/com/example/retrofitcuartos/SensoresYAdaptadores.java
+++ app/src/main/java/com/example/retrofitcuartos/SensoresYAdaptadores.java
@@ -1,16 +1,27 @@
 package com.example.retrofitcuartos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SensoresYAdaptadores extends AppCompatActivity {
     RecyclerView rcv;
+    List<Sensores> sensList = new ArrayList<>();
+    SensorsAdapter sns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensores_yadaptadores);
+        rcv = findViewById(R.id.sensorsitem);
+        sns = new SensorsAdapter(sensList);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+        rcv.setAdapter(sns);
+        rcv.setHasFixedSize(true);
     }
 }
