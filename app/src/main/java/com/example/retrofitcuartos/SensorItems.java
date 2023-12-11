1f356c0f1d482a151e51f983e6a744d4473c198f
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-12-11 09:58:05 -0600

    Agregue los colores del theme y empezar con view de sensores

---
+++ app/src/main/java/com/example/retrofitcuartos/SensorItems.java
@@ -0,0 +1,14 @@
+package com.example.retrofitcuartos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_items);
+    }
+}
