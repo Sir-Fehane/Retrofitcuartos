ba58614ec4c8e6f7a29af451ebcd14e2f0443d96
Emiliano Aguilar <emilianoa.aguilar17@gmail.com>
2023-12-13 16:22:50 -0600

    Vista Usuario

---
+++ app/src/main/java/com/example/retrofitcuartos/UsuariosVista.java
@@ -0,0 +1,14 @@
+package com.example.retrofitcuartos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuariosVista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuarios_vista);
+    }
+}
