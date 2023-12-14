fe66c148f31d281a405207de9167475a71b4f64b
Emiliano Aguilar <22170011@uttcampus.edu.mx>
2023-12-14 00:01:52 -0600

    a

--- app/src/main/java/com/example/retrofitcuartos/models/Cuartos.java
+++ app/src/main/java/com/example/retrofitcuartos/models/Cuartos.java
@@ -4,34 +4,6 @@ public class Cuartos {
     private String id;
     private String nombre;
     private String propietario;
+    private String ruta;
 
-    public Cuartos(String id, String nombre, String propietario) {
-        this.nombre = nombre;
-        this.propietario = propietario;
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPropietario() {
-        return propietario;
-    }
-
-    public void setPropietario(String propietario) {
-        this.propietario = propietario;
-    }
 }
--- app/src/main/java/com/example/retrofitcuartos/repository/CuartosRepository.java
+++ app/src/main/java/com/example/retrofitcuartos/repository/CuartosRepository.java
@@ -1,5 +1,14 @@
 package com.example.retrofitcuartos.repository;
 
+import com.example.retrofitcuartos.retrofit.RetrofitClient;
+
+import retrofit2.Retrofit;
+
 public class CuartosRepository {
+public Retrofit retrofit;
+void setRetrofit(){
+    retrofit = RetrofitClient.getRetrofitClient();
+}
+
 
 }
