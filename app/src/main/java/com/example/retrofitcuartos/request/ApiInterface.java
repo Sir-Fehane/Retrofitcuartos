5002945780f189d9a9a3d65d4ece401e9316e4e5
Emiliano Aguilar <emilianoa.aguilar17@gmail.com>
2023-12-14 19:33:34 -0600

    Usuarios Listo, falta el jwt

--- app/src/main/java/com/example/retrofitcuartos/request/ApiInterface.java
+++ app/src/main/java/com/example/retrofitcuartos/request/ApiInterface.java
@@ -2,6 +2,7 @@ package com.example.retrofitcuartos.request;
 
 import com.example.retrofitcuartos.models.Cuartos;
 import com.example.retrofitcuartos.models.Sensores;
+import com.example.retrofitcuartos.models.Usuarios;
 
 import java.util.List;
 
@@ -13,4 +14,6 @@ import retrofit2.http.Path;
 public interface ApiInterface {
     @GET("auth/recibir-datos")
     Call<List<Cuartos>> getCuartos(@Header("Authorization") String authorizationHeader);
+    @GET("auth/profile")
+    Call<Usuarios> obtenerUsuario();
 }
